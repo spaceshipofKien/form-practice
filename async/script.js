c59cc76f446e84f67dbdee72689090283a9a0595
spaceshipofKien <kien99.workspace@gmail.com>
2025-02-09 07:44:31 +0700

    async

--- async/script.js
+++ async/script.js
@@ -5,6 +5,36 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
+const allCategories = {
+  299: { category: "Career Advice", className: "career" },
+  409: { category: "Project Feedback", className: "feedback" },
+  417: { category: "freeCodeCamp Support", className: "support" },
+  421: { category: "JavaScript", className: "javascript" },
+  423: { category: "HTML - CSS", className: "html-css" },
+  424: { category: "Python", className: "python" },
+  432: { category: "You Can Do This!", className: "motivation" },
+  560: { category: "Backend Development", className: "backend" },
+};
+
+const forumCategory = (id) => {
+  let selectedCategory = {};
+
+  if (allCategories.hasOwnProperty(id)) {
+    const { className, category } = allCategories[id];
+
+    selectedCategory.className = className;
+    selectedCategory.category = category;
+  } else {
+    selectedCategory.className = "general";
+    selectedCategory.category = "General";
+    selectedCategory.id = 1;
+  }
+  const url = `${forumCategoryUrl}${selectedCategory.className}/${id}`;
+  const linkText = selectedCategory.category;
+  const linkClass = `category ${selectedCategory.className}`;
+  return `<a href="${url}" ></a>`;
+};
+
 const timeAgo = (time) => {
   const currentTime = new Date();
   const lastPost = new Date(time);
@@ -25,6 +55,16 @@ const timeAgo = (time) => {
   }
 
   return `${daysAgo}d ago`;
+};
+
+const viewCount = (views) => {
+  const thousands = Math.floor(views / 1000);
+
+  if (views >= 1000) {
+    return `${thousands}k`;
+  }
+
+  return views;
 };
 
 const fetchData = async () => {
@@ -62,10 +102,9 @@ const showLatestPosts = (data) => {
       </td>
       <td></td>
       <td>${posts_count - 1}</td>
-      <td>${views}</td>
-      <td>
-      
-      </td>
+      <td>${viewCount(views)}</td>
+      <td>${timeAgo(bumped_at)}</td>
     </tr>`;
   }).join("");
 };
+
